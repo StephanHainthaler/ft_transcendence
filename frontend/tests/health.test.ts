7a632a8cce53d9dd968051ef54a90f9f34be8321
Victor Vobis <victor@victorvobis.org>
2025-11-06 17:55:01 +0100

    small bug fix in test

--- frontend/tests/health.test.ts
+++ frontend/tests/health.test.ts
@@ -3,15 +3,14 @@ import assert from "node:assert";
 
 const API_URL = process.env.VITE_API_URL;
 
-test('backend health', async () => {
-  const response = await fetch(`${API_URL}/api/health/`, {
+test("backend health", async () => {
+  const response = await fetch(`${API_URL}/health`, {
     method: "GET",
   });
-
   if (response.ok) {
     const healthData = await response.json();
-    assert.strictEqual(healthData.status, 'healthy');
+    assert.strictEqual(healthData.status, "healthy");
   } else {
     throw new Error("Health response not okay");
   }
-})
+});
