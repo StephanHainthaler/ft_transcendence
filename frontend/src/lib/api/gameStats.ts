b2aaa7a32d9e5c60a0cdae82ea25be9036c71e21
Kateryna Huk <khuk@c3r5p4.42vienna.com>
2026-01-05 15:47:36 +0100

    game_stats/frontend: added game stats api service

--- frontend/src/lib/api/gameStats.ts
+++ frontend/src/lib/api/gameStats.ts
@@ -3,34 +3,35 @@ import { request } from "./utils";
 import type { Writable } from "@lib/types/writable";
 import type { JWT } from "@shared/api";
 
-async function fetchUserStats(tocken: Writable<JWT | null>, userId: number):  Promise<UserStats | null>
+async function fetchUserStats(token: Writable<JWT | null>, userId: number):  Promise<UserStats | null>
 {
-	try
-	{
-		const response = await fetch(`api/users/${userId}/stats`);
-		if(!response.ok)
-			return (console.error(`Service error:${response.status}`), null);
-		const data: UserStats = await response.json();
-		return (data);
-	} catch (error)
-	{
-		console.error("Network error:", error);
-		return (null);
-	}
+
+	const req = new Request(`/api/users/${userId}/stats`,
+		{
+			method: "GET",
+			headers: {'authorization': `Bearer ${ token.get()?.raw }`},
+		}
+	);
+	const response = await request(req, token);
+	const data = await response.json();
+	if (!response.ok)
+		throw (data);
+	return (data as UserStats);
 }
 
-async function fetchMatchHistory(userId: number) : Promise<MatchHistoryEntry[] | []>
+async function fetchMatchHistory(token: Writable <JWT | null>, userId: number) : Promise<MatchHistoryEntry[]>
 {
-try
-{
-	const response = await fetch(`api/users/${userId}/history`);
-	if (!response.ok) return [];
-	const data: MatchHistoryEntry[] = await response.json();
-	return data;
-} catch (error)
-{
-	return [];
-}
+	const req = new Request(`/api/users/${userId}/history`,
+		{
+			method: "GET",
+			headers: {'authorization': `Bearer ${ token.get()?.raw }`}
+		}
+	);
+	const response = await request(req, token);
+	const data = await response.json();
+	if (!response.ok) 
+		throw (data);
+	return (data as MatchHistoryEntry[]);
 }
 
 export { fetchUserStats, fetchMatchHistory };
